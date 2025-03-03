6c91f43e2163b3fbc8e0ae5e76c189beae614230
Vladislav Korzun <vladkorzun2016@yandex.ru>
2025-03-03 10:25:46 +0300

    Пытаюсь сделать дизайн. Пробую программный метод

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    QWidget *centralWidget = new QWidget(&w);
+    w.setCentralWidget(centralWidget);
+    centralWidget->setStyleSheet("background-color: #2E3440;");
     w.show();
     return a.exec();
 }
