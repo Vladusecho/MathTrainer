401d6ec396eb0dc04467d2d4b31653ca1a030d78
Vladislav Korzun <vladkorzun2016@yandex.ru>
2025-03-03 22:46:33 +0300

    Добавил кнопочки на главную заставку. Почти готово

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QLabel>
+#include <QStatusBar>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     MainWindow w;
     w.setStyleSheet("background-color:#A9A9A9;");
+
+    QLabel *author = new QLabel("Vladislav Korzun 2025   ", &w);
+    w.statusBar()->addPermanentWidget(author);
 
     w.show();
     return app.exec();
