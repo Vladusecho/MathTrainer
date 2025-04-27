ee23c4cd53987bc9337ce3f85bdc625f5eeaf693
Vladislav Korzun <vladkorzun2016@yandex.ru>
2025-04-28 00:03:37 +0300

    add register files

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     MainWindow w;
-    w.setStyleSheet("background-color:#A9A9A9;");
 
     QLabel *author = new QLabel("Vladislav Korzun 2025   ", &w);
     w.statusBar()->addPermanentWidget(author);
